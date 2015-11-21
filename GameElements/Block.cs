41ed2894148868c21a5a8d9200fe0750e84a8689
Callski <nathanmichalski@Nathans-MacBook-Pro.local>
2015-11-20 22:20:22 -0500

    Block now can move left and right

--- GameElements/Block.cs
+++ GameElements/Block.cs
@@ -10,16 +10,27 @@ namespace JumpGame
 		private Color color;
 
 		//Speed is pixels per second.
-		private int fallSpeed;
-		private int leftOverTime = 0;
+		private int xSpeed = 0;
+	    private int ySpeed = 0;
+		private int leftOverTimeX = 0;
+		private int leftOverTimeY = 0;
 
-		public Block(Jump game, Rectangle blockRect, Color color, int fallSpeed) : base(game)
+		public Block(Jump game, Rectangle blockRect, Color color, int ySpeed) : base(game)
 		{
 			this.game = game;
 			this.rect = blockRect;
 			this.color = color;
-			this.fallSpeed = (1000/fallSpeed);
-			Console.WriteLine("fallspeed: " + this.fallSpeed);
+//			this.xSpeed = xSpeed == 0 ? 0 : (1000 / xSpeed);
+			this.ySpeed = ySpeed == 0 ? 0 : (1000 / ySpeed);
+		}
+
+		public Block(Jump game, Rectangle blockRect, Color color, int xSpeed, int ySpeed) : base(game)
+		{
+			this.game = game;
+			this.rect = blockRect;
+			this.color = color;
+			this.xSpeed = xSpeed == 0 ? 0 : (1000 / xSpeed);
+			this.ySpeed = ySpeed == 0 ? 0 : (1000 / ySpeed);
 		}
 
 		public bool Hit
@@ -35,11 +46,8 @@ namespace JumpGame
 
 		public override void Update(GameTime gt)
 		{
-			int totalTime = (gt.ElapsedGameTime.Milliseconds + leftOverTime);
-			int dropAmount = totalTime / fallSpeed;
-
-			leftOverTime = totalTime % fallSpeed;
-			rect = new Rectangle(rect.X, rect.Y + dropAmount, rect.Width, rect.Height);
+			CalculateAndMoveX(gt);
+			CalculateAndMoveY(gt);
 
 			if (rect.Y > game.graphics.PreferredBackBufferHeight || rect.X > game.graphics.PreferredBackBufferHeight)
 			{
@@ -47,6 +55,41 @@ namespace JumpGame
 			}
 		}
 
+		public int GetXSpeed(GameTime gt)
+		{
+			int totalTime = (gt.ElapsedGameTime.Milliseconds + leftOverTimeX);
+			return totalTime / xSpeed;
+		}
+
+		public int GetYSpeed(GameTime gt)
+		{
+			int totalTime = (gt.ElapsedGameTime.Milliseconds + leftOverTimeY);
+			return totalTime / ySpeed;
+		}
+
+		private void CalculateAndMoveX(GameTime gt)
+		{
+			if (xSpeed == 0)
+				return;
+			
+			int totalTime = (gt.ElapsedGameTime.Milliseconds + leftOverTimeX);
+			int dropAmount = totalTime / xSpeed;
+
+			leftOverTimeX = totalTime % xSpeed;
+			rect = new Rectangle(rect.X + dropAmount, rect.Y, rect.Width, rect.Height);
+		}
+
+		private void CalculateAndMoveY(GameTime gt)
+		{
+			if (ySpeed == 0)
+				return;
+			
+			int totalTime = (gt.ElapsedGameTime.Milliseconds + leftOverTimeY);
+			int dropAmount = totalTime / ySpeed;
+
+			leftOverTimeY = totalTime % ySpeed;
+			rect = new Rectangle(rect.X, rect.Y + dropAmount, rect.Width, rect.Height);
+		}
 
 		private void CreateTexture(int width, int height, GraphicsDeviceManager g)
 		{
